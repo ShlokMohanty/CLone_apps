7fc5f2f9dbf97240fe99719fc1e760ec2603ccaa
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-08-04 23:04:01 +0530

    Update HomeScreen.js

--- netflix_clone_practice/src/features/HomeScreen.js
+++ netflix_clone_practice/src/features/HomeScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner";
+import Row from "./Row";
+import requests from "./Request";
 
-function HomeScreen(){
+function HomeScreen() {
     return ( 
     <div className="homeScreen">
         <Nav />
-        
-
-        < Banner />
+        <Banner />
         <Row 
         title='NETFLIX ORIGINALS'
         fetchUrl={requests.fetchNetflixOriginals}
