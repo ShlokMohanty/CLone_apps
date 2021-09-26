28de9a626ce8e4af1e15ede7f4c1632b08ed2ab0
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-09-26 22:47:18 +0530

    Create GildedRoseCBackstagePassesTest.java

---
+++ class_work(lidor)/src/test/java/com/a/introduction/gildedrose/GildedRoseCBackstagePassesTest.java
@@ -0,0 +1,28 @@
+package com.a.introduction.gildedrose;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+public class GildedRoseCBackstagePassesTest {
+  @Test
+  public void testupdateQualityBackstagePasses1() {
+    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 3);
+    Item[] items = new Item[] {item};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("Backstage passes to a TAFKAL80ETC concert",
+                app.items[0].name);
+    assertEquals(14, app.items[0].sellIn);
+    assertEquals(4, app.items[0].quality);
+  }
+  @Test 
+  public void testUpdateQualityBackstagePasses2() {
+    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 7, 3);
+    Item[] items = new Item[] {item};
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals("Backstage passes to a TAFKAL80ETC concert",
+                app.items[0].name);
+    assertEquals(3, app.items[0].sellIn);
+    assertEquals(6, app.items[0].quality);
+  }
+}
+    
